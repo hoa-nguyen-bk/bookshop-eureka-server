db9d7b95fbb9d94978d6db9df3e9fbb094ae0c39
hoa-nguyen-bk <haveaniceday6174@gmail.com>
2025-06-29 00:07:48 +0700

    Initialize Bookshop Eureka Server project with Maven and Spring Boot setup

---
+++ src/main/java/com/example/bookshop_eureka_server/BookshopEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookshop_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshopEurekaServerApplication.class, args);
+	}
+
+}
